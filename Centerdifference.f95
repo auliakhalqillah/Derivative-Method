cead888fd670e1ec49d6369906c9e7166741bfb1
auliakhalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2019-07-12 21:48:23 +0700

    Update Centerdifference.f95

--- Centerdifference.f95
+++ Centerdifference.f95
@@ -1,6 +1,6 @@
 ! HOW TO COMPILE THROUGH COMMAND LINE (CMD OR TERMINAL)
-! gfortran -c Centerdifference.f95
-! gfortran -o centerdiff Centerdifference.o
+! gfortran -c Backwarddifference.f95
+! gfortran -o backwarddiff Backwarddiference.o
 !
 ! The program is open source and can use to numeric study purpose.
 ! The program was build by Aulia Khalqillah,S.Si
@@ -8,45 +8,47 @@
 ! email: auliakhalqillah.mail@gmail.com
 ! ==============================================================================
 PROGRAM CenterDifference
-IMPLICIT NONE
+  IMPLICIT NONE
 
-REAL :: A,B,H
-REAL,DIMENSION(1000) :: F,FX,X
+  REAL :: A,B,H
+  REAL,DIMENSION(1000) :: F,FX,X
 
-INTEGER :: I,N
+  INTEGER :: I,N
+  CHARACTER(len=100) :: FMT
 
-WRITE(*,*) "INSERT INITIAL BOUNDARY:"
-read *, A
-WRITE(*,*) "INSERT FINAL BOUNDARY:"
-read *, B
-WRITE(*,*) "INSERT DATA LENGTH:"
-read*, N
+  WRITE(*,*)""
+  WRITE(*,*)"----------------------------------------"
+  WRITE(*,*)"CENTER DIFFERENCE METHOD - DERIVATIVE"
+  WRITE(*,*)"----------------------------------------"
+  WRITE(*,*) ""
+  WRITE(*,"(a)",advance="no") "INSERT INITIAL BOUNDARY:"
+  READ *, A
+  WRITE(*,"(a)",advance="no") "INSERT FINAL BOUNDARY:"
+  READ *, B
+  WRITE(*,"(a)",advance="no") "INSERT DATA LENGTH:"
+  READ *, N
 
-OPEN(10, FILE ='CenterOut.txt', STATUS='replace')
-! Calculating residual between data point
-H = (B-A)/N
+  FMT = "(a12,a13,a20,a20)"
+  WRITE(*,*) ""
+  WRITE(*,FMT)"ITER","Data(X)","Analytic F(X)","Integration F(X)"
+  OPEN(10, FILE ='CenterOut.txt', STATUS='replace')
+  ! Calculating residual between data point
+  H = (B-A)/N
 
-! Calculating First Orde Center Difference
-X(1) = A
-DO I = 1, N+1
-  F(I) = X(I)**2
-  X(I+1) = X(I) + H
-END DO
+  ! Calculating First Orde Center Difference
+  X(1) = A
+  DO I = 1, N+1
+    F(I) = X(I)**2
+    X(I+1) = X(I) + H
+  END DO
 
-DO I = 1,N
-  FX(I) = (F(I+1) - F(I-1))/(2*H)
-END DO
+  DO I = 1,N
+    FX(I) = (F(I+1) - F(I-1))/(2*H)
+  END DO
 
-WRITE(*,*) "------------------------------------------------------"
-WRITE(*,*) "First Column: Index Number"
-WRITE(*,*) "Second Column: Data Series"
-WRITE(*,*) "Third Column: Original Function Result"
-WRITE(*,*) "Fourth Column: Center Difference Function Result"
-WRITE(*,*) "------------------------------------------------------"
-
-DO I = 1,N
-  WRITE(*,*) I,X(I),F(I),FX(I)
-  WRITE(10,*) I,X(I),F(I),FX(I)
-END DO
+  DO I = 1,N
+    WRITE(*,*) I,X(I),F(I),FX(I)
+    WRITE(10,*) I,X(I),F(I),FX(I)
+  END DO
 
 END PROGRAM
